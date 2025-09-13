27b6d6197a1e39434ada780bda8a452fe941285f
lachlanshoesmith <12870244+lachlanshoesmith@users.noreply.github.com>
2025-09-13 22:46:01 +1000

    🎲 only add some msgs

--- bot.py
+++ bot.py
@@ -30,8 +30,9 @@ class Gork(discord.Client):
             return msg
 
     async def try_store_message(self, guild_id: str, message: discord.Message) -> None:
-        await self.db.lpush(guild_id, message.content)
-        await self.db.ltrim(guild_id, 0, 299)
+        if random.randint(0, 4) != 0:
+            await self.db.lpush(guild_id, message.content)
+            await self.db.ltrim(guild_id, 0, 299)
 
     async def on_message(self, message: discord.Message):
         if message.guild is None:
